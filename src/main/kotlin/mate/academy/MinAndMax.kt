69ce1a1b6650c82b9bfcd5d9139edd3fe442c1ba
Vitaliy Tishik <vitisik54@gmail.com>
2026-03-31 16:46:36 +0300

    init

--- src/main/kotlin/mate/academy/MinAndMax.kt
+++ src/main/kotlin/mate/academy/MinAndMax.kt
@@ -1,5 +1,15 @@
 package mate.academy
 
 fun getMinAndMax(inputArray: IntArray): IntArray {
+    if (arr.isEmpty()) return intArrayOf()
 
+    var min = arr[0]
+    var max = arr[0]
+
+    for (num in arr) {
+        if (num < min) min = num
+        if (num > max) max = num
+    }
+
+    return intArrayOf(min, max)
 }
